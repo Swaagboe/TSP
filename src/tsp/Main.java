7c787254e8cdbc4cb067e1c920b9b91f20967346
MaSommer <martisom@stud.ntnu.no>
2016-10-26 11:17:14 +0200

    finds minLong, minLat, maxLong and maxLat

--- src/tsp/Main.java
+++ src/tsp/Main.java
@@ -64,7 +64,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		new Main("Q");
+		new Main("WS");
 	}
 
 }
--- src/tsp/TSP.java
+++ src/tsp/TSP.java
@@ -2,10 +2,45 @@ package tsp;
 
 public class TSP {
 	
+	//[lat][long]
 	private double[][] cityCoordinates;
+	private double maxLat;
+	private double minLat;
+	private double maxLong;
+	private double minLong;
+	private final double INF = Double.MAX_VALUE;
+	
+	
 	
 	public TSP(double[][] cityCoordinates){
 		this.cityCoordinates = cityCoordinates;
+		findMaxLatLong();
+	}
+	
+	public void findMaxLatLong(){
+		double maxLat = -INF;
+		double maxLong = -INF;
+		double minLat = INF;
+		double minLong = INF;		
+		for (int i = 0; i < cityCoordinates.length; i++) {
+			if (cityCoordinates[i][0] > maxLat){
+				maxLat = cityCoordinates[i][0];
+			}
+			if (cityCoordinates[i][1] > maxLong){
+				maxLong = cityCoordinates[i][1]; 
+			}
+			if (cityCoordinates[i][0] < minLat){
+				minLat = cityCoordinates[i][0];
+			}
+			if (cityCoordinates[i][1] < minLong){
+				minLong = cityCoordinates[i][1];
+			}
+			
+		}
+		this.maxLat = maxLat;
+		this.maxLong = maxLong;
+		this.minLat = minLat;
+		this.minLong = minLong;
 	}
 
 }
